7d74c6758723b8d692430b48360309eb19e8e6d3
xinhecuican <lizilin2686462757@gmail.com>
2022-08-23 09:02:37 +0800

    initial commit

---
+++ entry/src/main/java/com/xinhecuican/qrcodescan/slice/MainAbilitySlice.java
@@ -0,0 +1,28 @@
+package com.xinhecuican.qrcodescan.slice;
+
+import com.xinhecuican.qrcodescan.ResourceTable;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+
+public class MainAbilitySlice extends AbilitySlice {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main);
+        initUI();
+    }
+
+    @Override
+    public void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    public void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
+
+    private void initUI()
+    {
+    }
+}
---
+++ entry/src/test/java/com/xinhecuican/qrcodescan/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.xinhecuican.qrcodescan;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
